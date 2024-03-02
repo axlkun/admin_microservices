9dec080e597d04ed09b90b13d6ca3a2b99ed1f91
axlkun <axelcruz.dev@gmail.com>
2024-03-02 13:37:50 -0600

    execute the jobs when updated or delete

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -3,6 +3,8 @@
 namespace App\Http\Controllers;
 
 use App\Jobs\ProductCreated;
+use App\Jobs\ProductDeleted;
+use App\Jobs\ProductUpdated;
 use App\Models\Product;
 use Illuminate\Http\Request;
 use Symfony\Component\HttpFoundation\Response;
@@ -32,12 +34,16 @@ class ProductController extends Controller
 
         $product->update($request->only('title','image'));
 
+        ProductUpdated::dispatch($product->toArray());
+
         return response($product, Response::HTTP_ACCEPTED);
     }
 
     public function destroy($id){
         
         Product::destroy($id);
+
+        ProductDeleted::dispatch($id);
 
         return response(null, Response::HTTP_NO_CONTENT);
     }
