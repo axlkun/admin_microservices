cb9cf91be797031080770b8cba898ac7f3ab353b
axlkun <axelcruz.dev@gmail.com>
2024-03-02 22:39:13 -0600

    catch event and update table

---
+++ app/Jobs/ProductLiked.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace App\Jobs;
+
+use App\Models\Product;
+use Illuminate\Bus\Queueable;
+use Illuminate\Queue\SerializesModels;
+use Illuminate\Queue\InteractsWithQueue;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Foundation\Bus\Dispatchable;
+
+class ProductLiked implements ShouldQueue
+{
+    use Dispatchable, InteractsWithQueue, Queueable, SerializesModels;
+
+    private $data;
+    /**
+     * Create a new job instance.
+     */
+    public function __construct($data)
+    {
+        $this->data = $data;
+    }
+
+    /**
+     * Execute the job.
+     */
+    public function handle(): void
+    {
+        $product = Product::find($this->data['product_id']);
+
+        $product->likes++;
+
+        $product->save();
+    }
+}
