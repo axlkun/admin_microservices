2130232c2f6bce5505724a78cbad0118c7bd757d
axlkun <axelcruz.dev@gmail.com>
2024-03-02 22:39:24 -0600

    execute product liked event

--- app/Providers/EventServiceProvider.php
+++ app/Providers/EventServiceProvider.php
@@ -2,10 +2,12 @@
 
 namespace App\Providers;
 
+use App;
+use App\Jobs\ProductLiked;
+use Illuminate\Support\Facades\Event;
 use Illuminate\Auth\Events\Registered;
 use Illuminate\Auth\Listeners\SendEmailVerificationNotification;
 use Illuminate\Foundation\Support\Providers\EventServiceProvider as ServiceProvider;
-use Illuminate\Support\Facades\Event;
 
 class EventServiceProvider extends ServiceProvider
 {
@@ -25,7 +27,7 @@ class EventServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        //
+        \App::bindMethod(ProductLiked::class . '@handle', fn($job) => $job->handle() );
     }
 
     /**
